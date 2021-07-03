89763b2398a07aefe57fac708d1295a23f656990
Michael Medrano <mgmedrano@outlook.com>
2021-07-02 20:48:11 -0700

    html updated, style added, script started

---
+++ script.js
@@ -0,0 +1,6 @@
+var quiz = document.getElementById("quiz");
+var ansOpt = document.getElementById("answerOptions");
+var ans1 = document.getElementById("ans1");
+var ans2 = document.getElementById("ans2");
+var ans3 = document.getElementById("ans3");
+var ans4 = document.getElementById("ans4");
