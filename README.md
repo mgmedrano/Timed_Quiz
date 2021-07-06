afb18e3697061009eb93b6dda06d0945aa1e06fd
Michael Medrano <mgmedrano@outlook.com>
2021-07-06 16:55:59 -0700

    HTML updated, script updated (struggle points), README done, Assets added for preview

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Timed_Quiz
+# Timed_Quiz: JavaScript Fundamentals (incomplete)
+
+## Background
+- As a coding boot camp student, I am making a timed quiz on JavaScript fundamentals. The goal is utilize the tools I have been provided with during class as well as outside resources. The quiz will also store high scores to allow myself to gauge my progress. 
+
+
+## Quiz Targets:
+- When the start button is clicked, a timer starts and the user is presented with a question and answers. 
+- When the question is answered (right or wrong), another question is presented
+- If the answer is incorrect, a penalty is applied by reducing the time.
+- When all questions are answered or the time is up, the game is over
+- At the end of game, the user can enter their initials and score to a leader board.
+
+
+# Quiz Preview 
+> Preview ![Quiz Start](Assets/javaScriptQuiz.png) 
+
+
+
+## Repository & Webpage:
+- URL: https://github.com/mgmedrano/Timed_Quiz
+- GitHub Repository: https://mgmedrano.github.io/Timed_Quiz/
--- script.js
+++ script.js
@@ -1,6 +1,75 @@
-var quiz = document.getElementById("quiz");
-var ansOpt = document.getElementById("answerOptions");
-var ans1 = document.getElementById("ans1");
-var ans2 = document.getElementById("ans2");
-var ans3 = document.getElementById("ans3");
-var ans4 = document.getElementById("ans4");
+var score = 0;
+var questionIndex = 0;
+var secondsLeft = 100;
+var holdInterval = 0;
+var penalty = 15;
+
+var currentTime = document.getElementById("currentTime");
+var timer = document.getElementById("startTime");
+var questionsAsked = document.getElementById("questionsAsked");
+
+var questions = [
+    {
+        quizQuestion: "True or False statements are what type of primitives?",
+        quizAnswers: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "booleans"
+    }
+    {
+        quizQuestion: "The first index of an array is _____________",
+        quizAnswers: ["-1", "0", "1", "2"],
+        correctAnswer: "0"
+    },
+];
+
+var container = document.getElementById("container");
+var questionList = document.createElement("ul");
+
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time remaining: " +secondsLeft;
+
+            if (secondsLeft = 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time is up!";
+            }
+        }, 1000);
+    });
+    render(questionIndex);
+    
+});
+
+function render(questionIndex) {
+    quizQuestion.textContent = " ";
+    quizAnswers.textContent = " ";
+
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].quizQuestion;
+        var userChoices = questions[questionIndex].quizAnswers;
+        questionsAsked.textContent = userQuestion;
+    }
+
+    userChoices.forEach(function (newItem) {
+        var listEl = document.createElement("li");
+        listEl.textContent = newItem;
+        questionsAsked.appendChild(questionList);
+        questionList.appendChild(listEl);
+        listEl.addEventListener("click", (compare));
+    })
+}
+ function compare(event) {
+     var element = event.target;
+     if(element.matches("li")) {
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        if(element.textContent == questions[questionIndex].correctAnswer) {
+            score++;
+            createDiv.textContent = "Correct!";
+        } else {
+            secondsLeft = secondsLeft-penalty;
+            createDiv.textContent = "You are incorrect";
+        }
+     }
+ };
