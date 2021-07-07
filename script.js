e042dfd5036cca5e0d30808e7fe6e69f07655a84
Michael Medrano <mgmedrano@outlook.com>
2021-07-06 23:11:26 -0700

    javascript updating functionality, not working

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var score = 0;
 var questionIndex = 0;
 var secondsLeft = 100;
-var holdInterval = 0;
 var penalty = 15;
 
 var currentTime = document.getElementById("currentTime");
@@ -25,8 +24,8 @@ var container = document.getElementById("container");
 var questionList = document.createElement("ul");
 
 timer.addEventListener("click", function () {
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function () {
+    if (secondsLeft === 100) {
+        timer = setInterval(function () {
             secondsLeft--;
             currentTime.textContent = "Time remaining: " +secondsLeft;
 
@@ -49,27 +48,5 @@ function render(questionIndex) {
         var userQuestion = questions[questionIndex].quizQuestion;
         var userChoices = questions[questionIndex].quizAnswers;
         questionsAsked.textContent = userQuestion;
-    }
-
-    userChoices.forEach(function (newItem) {
-        var listEl = document.createElement("li");
-        listEl.textContent = newItem;
-        questionsAsked.appendChild(questionList);
-        questionList.appendChild(listEl);
-        listEl.addEventListener("click", (compare));
-    })
-}
- function compare(event) {
-     var element = event.target;
-     if(element.matches("li")) {
-        var createDiv = document.createElement("div");
-        createDiv.setAttribute("id", "createDiv");
-        if(element.textContent == questions[questionIndex].correctAnswer) {
-            score++;
-            createDiv.textContent = "Correct!";
-        } else {
-            secondsLeft = secondsLeft-penalty;
-            createDiv.textContent = "You are incorrect";
-        }
-     }
- };
+        questionsAsked.textContent = userChoices;
+    };
